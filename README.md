085af04ffd8a8fbc0483f9877b596cc7c95e1b6e
TomRyan-321 <25472582+TomRyan-321@users.noreply.github.com>
2020-07-23 13:53:49 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Cloud-One-Conformity-Role-Updater
+
+## About
+
+Simple Python based Lambda function to keep your Cloud Conformity roles up to date whenever there is a new release available. 
+
+The template deploys a lambda function running on the Python 3.7 runtime with a scheduled event triggering the function once daily to check if the template deployed matches the latest available from the official template url. If a new version is available the function will automatically update the existing CloudConformity stack to the latest version.
+
+## Installation
+
+Deploy the cloudformation template in the same region you have deployed the CloudConformtiy role template (default and recommended region: us-east-1).
---
+++ src/role-updater.py
@@ -0,0 +1,46 @@
+import boto3
+import json
+import urllib3
+
+templateurl='https://s3-us-west-2.amazonaws.com/cloudconformity/CloudConformity.template'
+cfresource=boto3.resource('cloudformation')
+stack=cfresource.Stack('CloudConformity')
+stackparams=[
+    {
+        'ParameterKey': 'AccountId',
+        'UsePreviousValue': True
+    },
+    {
+        'ParameterKey': 'ExternalId',
+        'UsePreviousValue': True
+    }
+]
+
+def get_conformity_template_verion():
+    http=urllib3.PoolManager()
+    template=http.request('GET', templateurl)
+    templatejson=json.loads(template.data.decode('utf-8'))['Outputs']['Version']
+    for key, value in templatejson.items():
+        print('The latest CloudConformity template version is: {}'.format(value))
+        return(value)
+
+def get_conformity_stack_version():
+    stackoutputs=stack.outputs
+    for o in stackoutputs:
+        if o['OutputKey'] == 'Version':
+            print('The CloudConformity stack version is: {}'.format(o['OutputValue']))
+            return(o['OutputValue'])
+
+def main(event, context):
+    templateversion=get_conformity_template_verion()
+    stackversion=get_conformity_stack_version()
+    if templateversion > stackversion:
+        stack.update(TemplateURL=templateurl,UsePreviousTemplate=False,Parameters=stackparams,Capabilities=['CAPABILITY_NAMED_IAM'])
+        print('The CloudConformity stack is out of date, updating the stack to: {}'.format(templateversion))
+        return('The CloudConformity stack is out of date, updating the stack to: {}'.format(templateversion))
+    else:
+        print('The CloudConformity stack is already up to date, version: {}'.format(stackversion))
+        return('The CloudConformity stack is already up to date, version: {}'.format(stackversion))
+
+if __name__ == "__main__":
+    main(event=None, context=None)
